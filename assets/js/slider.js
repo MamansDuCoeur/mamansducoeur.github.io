bf95731f79f97d392cfd2ec5fc068f8525bf1c4c
enzoclock <enzo.testa@oclock.io>
2022-12-22 17:55:54 +0100

    un petit coup de propre

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -5,29 +5,32 @@ function changeBackgroundImage() {
     
     home.style.backgroundImage = `
         linear-gradient(rgba(0, 0, 0, .5), rgba(0, 0, 0, .5)),
-        url("assets/images/groupe${nextImage}.jpg")
+        url("assets/images/groupes/groupe${nextImage}.jpg")
     `; // On change la background imageavec la nouvelle valeur
 
 
     // Selectionner le point qui a le cercle REMPLI pour le transformer en cercle VIDE
-    const cerclePlein = document.querySelector(".fa-solid.fa-circle");
+    let cerclePlein = document.querySelector(".fa-solid.fa-circle");
     cerclePlein.classList.remove("fa-solid");
     cerclePlein.classList.add("fa-regular");
 
     // Selectionner le point qui correspond à la nouvelle image, et le transformer en cercle PLEIN
-    const cercleContainer = document.querySelector(".slider-buttons");
-    const prochainCercle = cercleContainer.children[nextImage - 1];
+    let cercleContainer = document.querySelector(".slider-buttons");
+    let prochainCercle = cercleContainer.children[nextImage - 1];
     prochainCercle.classList.remove("fa-regular");
     prochainCercle.classList.add("fa-solid");
 
+    // Puis on prepare le prochain appel à la fonction
     nextImage = nextImage + 1; // On met à jour la next image pour le prochain appel
 
     if (nextImage === 7) { // En revanche, si la nextImage vaut 7 (ce qui ne doit pas être le cas car on n'a que 6 image)...
-        nextImage = 1; // ..alors on réaffichera l'image 1
+        nextImage = 1; // ...alors on réaffichera l'image 1
     }
 }
 
 setInterval(changeBackgroundImage, 10 * 1000); // Toutes les 10 secondes, on change l'image de fond (1000 milliseconds = 10 secondes).
+
+// ===== RENDRE LES CERCLES DU SLIDDERS CLIQUABLE ====
 
 // On va selectionner TOUS les boutons !!
 const buttons = document.querySelectorAll(".slider-buttons i");
@@ -38,10 +41,8 @@ buttons.forEach((button, index) => {
     // le deuxieme bouton a l'index 1
     // etc....
 
-    // On écoute le click sur le bouton ! 
-    button.addEventListener("click", () => {
-        // Change la next image 
-        nextImage = index + 1;
-        changeBackgroundImage();
+    button.addEventListener("click", () => { // On écoute le click sur le bouton ! 
+        nextImage = index + 1; // On met à jour la prochaine image...
+        changeBackgroundImage(); // ...que l'on affiche immédiatement
     });
 });

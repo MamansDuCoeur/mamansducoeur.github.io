3f85140b5a24f9e331f8cb435af2bc806011fcfb
alexisOclock <alexis@oclock.io>
2023-01-19 11:44:44 +0100

    js fix

--- assets/js/submenu.js
+++ assets/js/submenu.js
@@ -1,7 +1,6 @@
 
 // On indique qu'il existe une fonction qui aura un comportement (ce qui est écirt entre les accolade) qui pourra être éxécuté lorsqu'on l'appelera
 function toggleSubMenu(){
-
     // on récupère l'élément qui a l'id submenu dans le document HTML
     let subMenuElem = document.querySelector("#submenu");
 
@@ -12,17 +11,25 @@ function toggleSubMenu(){
     subMenuElem.classList.toggle("hide");
 }
 
-function toggleArrow(){
-
+function hideeSubMenu(){
     // on récupère l'élément qui a l'id submenu dans le document HTML
-    let arrow = document.querySelector("#dropdown-arrow");
+    let subMenuElem = document.querySelector("#submenu");
 
-    // On demande à l'élément de "toggle" sur la classe hide
-    // c'est a dire que si elle est présente on supprime
-    // si elle absente on l'ajoute
-    // classList permet de connaitre les class présente dans un élément du cdocument
-    arrow.classList.toggle("hide");
+    // On demande à enlever la classe hide
+    subMenuElem.classList.remove("hide");
 }
+
+// function toggleArrow(){
+
+//     // on récupère l'élément qui a l'id submenu dans le document HTML
+//     let arrow = document.querySelector("#dropdown-arrow");
+
+//     // On demande à l'élément de "toggle" sur la classe hide
+//     // c'est a dire que si elle est présente on supprime
+//     // si elle absente on l'ajoute
+//     // classList permet de connaitre les class présente dans un élément du cdocument
+//     arrow.classList.toggle("hide");
+// }
 
 
 
@@ -31,8 +38,8 @@ function toggleArrow(){
 let presentationElement = document.querySelector("#presentation");
 let submenu = document.querySelector("#submenu");
 
-presentationElement.addEventListener("mouseover", toggleSubMenu);
-submenu.addEventListener("mouseout", toggleSubMenu);
+presentationElement.addEventListener("click", toggleSubMenu);
+submenu.addEventListener("mouseout", hideeSubMenu);
 
-presentationElement.addEventListener("mouseover", toggleArrow);
-presentationElement.addEventListener("mouseout", toggleArrow)
+// presentationElement.addEventListener("mouseover", toggleArrow);
+// presentationElement.addEventListener("mouseout", toggleArrow)
